fddbc96f80a759ecf23456b743e231d3ec854d78
Roger Tuan <r.tuan@datocms.com>
2024-11-18 11:46:34 -0800

    No classnames

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
                 <h1>Next 15</h1>
                 <h2>With the correct boolean type</h2>
                 <p>This will NOT loop:</p>
-                <MuxPlayer playbackId="LpkfsKWW7w02VfRMSlI5ekJlFskvF4Uyk" loop={true} className="video"/>
+                <MuxPlayer playbackId="LpkfsKWW7w02VfRMSlI5ekJlFskvF4Uyk" loop={true}/>
 
                 <h2>With the incorrect string type</h2>
                 <p>This WILL loop</p>
-                <MuxPlayer playbackId="LpkfsKWW7w02VfRMSlI5ekJlFskvF4Uyk" loop={"true"} className="video"/>
+                <MuxPlayer playbackId="LpkfsKWW7w02VfRMSlI5ekJlFskvF4Uyk" loop={"true"}/>
             </main>
         </div>
     );
