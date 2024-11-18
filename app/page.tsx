43c3818d6197cac474f4f22c380906f1fa7ebfe9
Roger Tuan <r.tuan@datocms.com>
2024-11-18 11:44:06 -0800

    Add h1

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,12 @@ export default function Home() {
     return (
         <div className={styles.page}>
             <main className={styles.main}>
-                <h1>With the correct boolean type</h1>
+                <h1>Next 15</h1>
+                <h2>With the correct boolean type</h2>
                 <p>This will NOT loop:</p>
                 <MuxPlayer playbackId="LpkfsKWW7w02VfRMSlI5ekJlFskvF4Uyk" loop={true} className="video"/>
 
-                <h1>With the incorrect string type</h1>
+                <h2>With the incorrect string type</h2>
                 <p>This WILL loop</p>
                 <MuxPlayer playbackId="LpkfsKWW7w02VfRMSlI5ekJlFskvF4Uyk" loop={"true"} className="video"/>
             </main>
